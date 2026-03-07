0e55933d01409a3fe5428496aedf15a24a285f35
Sqealbert <hddenherder@gmail.com>
2026-03-06 21:31:46 -0600

    Changed player sprite to sweet_baby.png and made it flip based off last direction moved. Added hearts and damage flash as well as a basic title screen/main menu/

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
+const HEART_SCENE = preload("res://scenes/heart.tscn")
+
 @export var speed: int = 400
 #h is short for horizontal
+@export var max_health: int
+@onready var current_health: int = max_health
+
 var h_direction: float
 var facing: float = -1.0
 
@@ -15,11 +20,21 @@ signal attack(dir: Vector2)
 @onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
 @onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
+func _ready() -> void:
+	set_hearts(current_health)
+	
 func _physics_process(delta: float) -> void:
 	get_input()
 	velocity.y += get_player_gravity() * delta
 	velocity.x = h_direction * speed
 	move_and_slide()
+
+func set_hearts(value: int):
+	for heart in $HeartContainer/HFlowContainer.get_children():
+		heart.queue_free()
+	for i in value:
+		var heart = HEART_SCENE.instantiate()
+		$HeartContainer/HFlowContainer.add_child(heart)
 
 func get_player_gravity():
 	return jump_gravity if velocity.y < 0.0 else fall_gravity
@@ -32,6 +47,16 @@ func get_input():
 		jump()
 	if Input.is_action_just_pressed("attack"):
 		attack.emit(Vector2(facing,0))
+		
+	$Sprite2D.flip_h = true if facing == 1.0 else false
 	
 func jump():
 	velocity.y = jump_velocity
+
+func hit():
+	$AnimationPlayer.play("damage_flash")
+	current_health -= 1
+	set_hearts(current_health)
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	hit()
